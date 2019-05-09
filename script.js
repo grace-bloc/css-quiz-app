dfa6b97ba37591c73a3aada208df4b5e4e4ac905
Grace Cupat <grace.cupat@bloc.io>
2019-05-09 06:51:21 -0700

    Updating JS

--- script.js
+++ script.js
@@ -124,13 +124,11 @@ function updateScore(){
 function renderResults(){
   if(score >= 8){
     $('.questionAnswerForm').html(`<div class="results correctFeedback"><h3>You are a true  Jedi &lt;CSS&gt; Master</h3><img src="https://ui-ex.com/images/yoda-transparent-star-wars-4.png" alt="Yoda" width="200px" /><p>You store is ${score} / 10</p><p>You are ready to face your destiny!</p><button class="restartButton">Restart your test</button></div>`);
-   /* } else if (score < 8 && score >= 5) { $('questionAnswerForm').html(`<div class="results correctFeedback"><h3>You are almost ready Padawan, but you will need to practice more</h3><img scr="https://studio.code.org/v3/assets/piudC0FcXxuaMPZszFjoDYplAEKMHcoBaV3JWB1lGzw/rey.png" alt="Trainee"/><p>You stored: ${score} / 10</p><p>Continue to hone your skills</p><button class="restartButton">Restart your test</button></div>`);*/
 
   }else if (score < 8 && score >= 5) {
     $('.questionAnswerForm').html(`<div class="results correctFeedback"><h3>You have trained well Padawan, but you will need more practice.</h3><img src="https://studio.code.org/v3/assets/piudC0FcXxuaMPZszFjoDYplAEKMHcoBaV3JWB1lGzw/rey.png" width="150px" alt="Trainee"/><p>You got ${score} / 10</p><p>Continue to prefect your skills</p><button class="restartButton">Restart Quiz</button></div>`);
   
     } else {
-      /*$('questionAnswerForm').html(`<div class="results correctFeedback"><h3>You will need more practice Padawan.</h3><img scr="https://upload.wikimedia.org/wikipedia/en/thumb/5/58/GeneralGrievous.png/220px-GeneralGrievous.png" alt="General" /><p>You stored: ${score} / 10</p><p>Continue to hone your skills</p><button class="restartButton">Restart your test</button></div>`);*/
 
       $('.questionAnswerForm').html(`<div class="results correctFeedback"><h3>You will need more practice Padawan.</h3><img src="https://upload.wikimedia.org/wikipedia/en/thumb/5/58/GeneralGrievous.png/220px-GeneralGrievous.png" width="200px" alt="General"/><p>You got ${score} / 10</p><p>You will face your denstiny soon enough!</p><button class="restartButton">Restart Quiz</button></div>`);
     }
